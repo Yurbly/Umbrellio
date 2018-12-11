a25f8d546b19b5693ff5c6315565a7687d639e2e
Iurii Novikov <yunov1@gmail.com>
2018-12-11 13:38:38 +0300

    Webpack config is set up

---
+++ client/webpack.config.js
@@ -0,0 +1,90 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
+const sourceRoot = './js';
+module.exports = {
+    target: "web",
+    context: __dirname,
+    mode: "development",
+    entry: `${sourceRoot}/index.tsx`,
+    output: {
+        path: path.resolve(__dirname, "/dist"),
+        filename: "bundle.js",
+        publicPath: "/dist/assets/"  //prefixes publicPath with 'localhost:9000/'
+        /*This is an important option when using on-demand-loading or loading
+        external resources like images, files, etc. If an incorrect value is
+        specified you'll receive 404 errors while loading these resources.
+
+        This option specifies the public URL of the output directory when referenced
+        in a browser. A relative URL is resolved relative to the HTML page (or <base> tag).
+        Server-relative URLs, protocol-relative URLs or absolute URLs are also possible and sometimes required,
+        i. e. when hosting assets on a CDN.*/
+    },
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    module:{
+        rules:[
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            insertAt: 'top' //todo
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        }
+                    },
+                    {
+                        loader: 'less-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'awesome-typescript-loader'
+                }
+            }
+        ]
+    },
+    devServer: {
+        publicPath: "/dist/assets/",  // relative path converts to localhost:9000/ + publicPath (for bundled files)
+        hot: true,
+        port: 9000,
+        contentBase: path.resolve(__dirname, sourceRoot),
+        compress: true,
+    }
+    ,
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js', '.jsx', '.less' ]
+    },
+    plugins: [
+        new CheckerPlugin(),
+        new HtmlWebpackPlugin({
+            template: `${sourceRoot}/index.html`,
+            inject: "body"
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    optimization: {
+        minimizer: [new UglifyJsPlugin()]
+    }
+};
