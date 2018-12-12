7cd7baf0662d4f722ed4a55c1dd3424669d7fda0
Iurii Novikov <yunov1@gmail.com>
2018-12-12 18:10:11 +0300

    Get method is implemented

--- server/App/Routes/todoRoutes.tsx
+++ server/App/Routes/todoRoutes.tsx
@@ -1,4 +1,4 @@
-
+const ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
     app.post('/todos', (req, res) => {
@@ -8,6 +8,17 @@ module.exports = function(app, db) {
                 res.send({ 'error': 'An error has occurred' });
             } else {
                 res.send(result.ops[0]);
+            }
+        });
+    });
+    app.get('/todos/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('todos').findOne(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(item);
             }
         });
     });
